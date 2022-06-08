bfcc393f61cd5397b486a49f9977f7b5a8bb252d
hugogriffithsappmakers <hugo.griffiths@appmakers.xyz>
2022-06-09 00:57:15 +0800

    Made my charts black and white

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-//Here we're importing items we'll need. You can add other imports here.
-
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
-};
-
-//
+import c3 from "c3";
+var chart = c3.generate({
+  bindto: '#chart',
+  data: {
+    type: "bar",
+    colors: {
+      WorkaholicLightGrey: '#c5c5c7',
+      'Workaholic Dark Grey': '#8a8a8a',
+  },
+    columns: [
+      ['WorkaholicLightGrey', 100, 200, 100, 400, 150, 250],
+      ['Workaholic Dark Grey', 50, 60, 70, 80, 90, 100]
+    ]
+  }
+});
