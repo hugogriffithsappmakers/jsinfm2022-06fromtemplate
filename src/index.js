8a9cf15c85a15359a2f8ff9ebb6bf80e8bd3558b
hugogriffithsappmakers <hugo.griffiths@appmakers.xyz>
2022-06-15 01:42:29 +0800

    Setup onclick passing data into FileMaker

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import c3 from "c3";
-var chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    type: "bar",
-    colors: {
-      WorkaholicLightGrey: '#c5c5c7',
-      'Workaholic Dark Grey': '#8a8a8a',
-  },
-    columns: [
-      ['WorkaholicLightGrey', 100, 200, 100, 400, 150, 250],
-      ['Workaholic Dark Grey', 50, 60, 70, 80, 90, 100]
-    ]
-  }
-});
+
+window.loadChart = function (param) {
+console.log(param);
+const obj = JSON.parse(param);
+const json = obj.data;
+const type = obj.type;  
+const padAngle = obj.padAngle;  
+console.log(obj)
+console.log(json)
+console.log(type)
+console.log(padAngle)
+  console.log(json);
+  //return
+  var chart = c3.generate({
+    bindto: '#chart',
+    axis: { x: { type: "category" } },
+    pie: { padAngle: padAngle},
+    data: {
+        //onclick: function (d) {alert("Clicked"), FileMaker.PerformScript("Get Data from JS", "Jeremy")},
+        onclick: function (d) {alert("Clicked"), FileMaker.PerformScript("Get Data from JS", JSON.stringify(d))},
+      type: type,
+      keys: { x: "month", value: ["apples", "peaches", "bananas"] },
+      colors: {
+        WorkaholicLightGrey: '#c5c5c7',
+        'Workaholic Dark Grey': '#8a8a8a',
+    },
+    json: json
+    }
+  });
+}
